5ee6faab3a15221a97f7eed0e83681cfdfc026ff
Daniel Koch <daniel.koch@helsinki.fi>
2023-11-23 19:49:36 +0200

    init

---
+++ Source/Skeeper/SkeeperGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SkeeperGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SKEEPER_API ASkeeperGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
